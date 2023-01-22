6d799b66a39f4ece09dcf5cda1f64897632b7171
htseee <html.se@yandex.ru>
2023-01-22 19:59:55 +0700

    added class media book movie cd

--- comment.js
+++ comment.js
@@ -490,3 +490,103 @@ nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications);
 const doctorOly = new Doctor('Oly', ['rd', 'rf']);
 console.log(doctorOly.name);
+
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._rating = [];
+  }
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  set isCheckedOut(checkedOut) {
+    this._isCheckedOut = checkedOut;
+  }
+  get rating() {
+    return this._rating;
+  }
+  getAverageRating() {
+    let sumRating = this._rating.reduce((currentSum, rating) => currentSum + rating, 0);
+    return sumRating/this._rating.length;
+  }
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+  addRating(r) {
+    this._rating.push(r);
+  }
+}
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director() {
+    return this._director;
+  }
+  get runTime() {
+    return this._runTime;
+  }
+}
+class CD extends Media {
+  constructor(artist, title, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+  get artist() {
+    return this._artist;
+  }
+  get songs() {
+    return this._songs;
+  }
+}
+class Catalog {
+  constructor() {
+    this._media = [];
+  }
+  addMedia(newMedia) {
+    this._media.push(newMedia);
+  }
+}
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
+const CDB = new CD('CDB', 'TrTrTr', ['TrTrTr', 'FfFfFf', 'DfDfDf']);
+CDB.addRating(1);
+CDB.addRating(5);
+CDB.addRating(5);
+console.log(CDB.getAverageRating());
+const arrayCatalog = new Catalog();
+arrayCatalog.addMedia(historyOfEverything);
+arrayCatalog.addMedia(speed);
+arrayCatalog.addMedia(CDB);
